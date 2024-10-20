bdabb77dbbc86a78554fd310e44ab4eca39773f8
İbrahim Erat <ibrahimerat80@gmail.com>
2024-10-20 22:42:44 +0200

    Improved algorithm

--- DivisorsLib/DivisorsLib.cpp
+++ DivisorsLib/DivisorsLib.cpp
@@ -4,10 +4,21 @@
 std::vector<unsigned long long> divisors(unsigned long long number)
 {
 	std::vector<unsigned long long> result;
-	for (unsigned long long i = 1; i <= number; ++i)
+	unsigned long long sqrtNumber = std::sqrt(number);
+
+	for (unsigned long long i = 1; i <= sqrtNumber; ++i)
 	{
 		if (number % i == 0)
+		{
 			result.push_back(i);
+			if (i != number / i)
+			{
+				result.push_back(number / i);
+			}
+		}
 	}
+
+
+	std::sort(result.begin(), result.end());
 	return result;
 }
